61d4b775dc3ac994ee37cd09b453f67e64713124
Jangmyun <cndlsrb2739@gmail.com>
2023-09-24 10:56:11 +0900

    원을 12개 그리는 함수 추가

--- canvas.js
+++ canvas.js
@@ -5,8 +5,31 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
-drawTriangle(20,50);
-drawSmile()
+
+
+function drawArcs(){
+    for(let i=0; i<4; i++){
+        for(let j=0; j<3; j++){
+            ctx.beginPath();
+            const x= 25 + j*50;
+            const y = 25+ i*50;
+            const radius = 20;
+            const startAngle = 0;
+            const endAngle = Math.PI + (Math.PI * j) / 2;
+            const counterclockwise = i % 2 !== 0
+
+            ctx.arc(x,y,radius,startAngle,endAngle,counterclockwise);
+            if(i>1) {
+                ctx.fill();
+            }else{
+                ctx.stroke(); 
+            }
+            
+        }
+    }
+}
+
+
 function drawTriangle(x,y){
     ctx.beginPath();
     ctx.moveTo(x,y);
@@ -28,5 +51,6 @@ function drawSmile(){
     ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
     ctx.stroke();
 }
+
 
 console.log(canvas);
